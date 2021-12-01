50e96086bb64fdadb8f971a47b42024e1d8a7f00
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-01 14:12:00 +0100

    Add mudBlazor

--- WizardWorldApp/WizardWorldApp/Program.cs
+++ WizardWorldApp/WizardWorldApp/Program.cs
@@ -7,12 +7,16 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+
 
 namespace WizardWorldApp {
     public class Program {
         public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddMudServices();
 
             builder.Services.AddScoped(
                 sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});

904406bf2fe3b535d6e8ad28fa8a2829d603a6d7
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-03 15:05:37 +0100

    add tables

---
+++ WizardWorldApp/WizardWorldApp/Data/FeedbackMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WizardWorldApp.Data {
+    public class FeedbackMessage {
+        public FeedbackType FeedbackType { get; set; }
+        public string Feedback { get; set; }
+        public Guid? EntityId { get; set; }
+    }
+    public enum FeedbackType {
+        General = 0,
+        Bug,
+        DataError,
+        Suggestion
+    }
+}
---
+++ WizardWorldApp/WizardWorldApp/Data/House.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WizardWorldApp.Data {
+    public class House {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string HouseColours { get; set; }
+        public string Founder { get; set; }
+        public string Animal { get; set; }
+        public string Element { get; set; }
+        public string Ghost { get; set; }
+        public string CommonRoom { get; set; }
+        public ICollection<HouseHead> Heads { get; set; }
+        public ICollection<Trait> Traits { get; set; }
+    }
+    public class HouseHead {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid HouseId { get; set; }
+    }
+    public class Trait {
+        public Guid Id { get; set; }
+        public TraitName Name { get; set; }
+        public Guid HouseId { get; set; }
+    }
+    public enum TraitName {
+        None = 0
+    }
+}

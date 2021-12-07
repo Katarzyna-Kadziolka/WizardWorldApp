5ac3847540075b5813296ecf10aea8687f5e3761
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-07 18:50:23 +0100

    Rest Sharp

---
+++ WizardWorldApp/WizardWorldApp/WizardWorldClient.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp;
+
+namespace WizardWorldApp {
+    public class WizardWorldClient {
+        private RestClient _client;
+
+        public WizardWorldClient() {
+            _client = new RestClient("https://wizard-world-api.herokuapp.com");
+        }
+        
+        
+    }
+}

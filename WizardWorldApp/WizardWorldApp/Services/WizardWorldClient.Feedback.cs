43b89fba25bec4d1a710688d24fdf1f915406357
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-12-08 14:59:11 +0100

    feedback

--- WizardWorldApp/WizardWorldApp/Services/WizardWorldClient.Feedback.cs
+++ WizardWorldApp/WizardWorldApp/Services/WizardWorldClient.Feedback.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Net.Http.Json;
 using System.Threading.Tasks;
 using WizardWorldApp.Data.Feedback;
-using WizardWorldApp.Extensions;
 
 namespace WizardWorldApp.Services {
     public partial class WizardWorldClient {
         public async Task PostFeedbackAsync(FeedbackCommand command) {
-            var query = new Dictionary<string, string>();
-            if (command.Feedback is not null) query.Add("Name", command.Feedback);
-            query.Add("SpellType", command.FeedbackType.ToString());
-            if (command.EntityId is not null) query.Add("Incantation", command.EntityId.ToString());
-            var response = await _client.GetAsync("Feedback", query);
+            var response = await _client.PostAsJsonAsync("Feedback", command);
         }
     }
 }
